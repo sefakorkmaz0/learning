2c28f3763f48fd521f0742009a697c2346a9f6d9
Sefa <sefa34korkmaz@gmail.com>
2026-06-27 14:00:54 +0300

    first commit

---
+++ typescript/interface.ts
@@ -0,0 +1,78 @@
+// interface book {
+//     title: string,
+//     pageCount: number,
+//     author: {
+//         name: string,
+//         birthYear: number
+//     };
+// }
+// const Of_Mice_and_Men: book = {
+//     title: "Of Mice and Men",
+//     pageCount: 132,
+//     author: {
+//         name: "John Steinbeck",
+//         birthYear: 1937
+//     }
+// }
+// console.log(Of_Mice_and_Men)
+
+
+
+
+
+
+
+// interface Order {
+//     orderNo: number,
+//     customer: {
+//         name: string,
+//         address: {
+//             city: string,
+//             postalCode: string
+//         }
+//     }
+// }
+// const bouquet: Order = {
+//     orderNo: 1,
+//     customer: {
+//         name: "sefa",
+//         address: {
+//             city: "Rize",
+//             postalCode: "84344"
+//         }
+//     }
+// }
+// console.log(bouquet)
+
+
+
+
+
+
+
+// interface post {
+//     id: number,
+//     title: string,
+//     body: string,
+//     userId?: number
+// }
+// const post1: post = {
+//     id: 142,
+//     title: "Signal and System",
+//     body: "This article provide to sound and attributes analizy for machine learning. ",
+// }
+// console.log(post1)
+
+
+
+
+
+
+
+// interface traffic_lamb {
+//     renk: "green" | "yellow" | "red"
+// }
+// const traffic: traffic_lamb = {
+//     renk: "red"
+// }
+// console.log(traffic)
